63cb66abc6e2f3550b394771dbbabf1b8586d254
TorchedVIP <torchedvip@gmail.com>
2025-12-08 18:12:43 -0700

    Fixed minor issue

--- main.py
+++ main.py
@@ -1,2 +1,3 @@
 # You must use a certain number somewhere in this code to solve the challenge.
-n,r=int(6),1;[r:=r*i for i in range(1,n+1)];print(r)
+
+n,r=int(0),1;[r:=r*i for i in range(1,n+1)];print(r)
